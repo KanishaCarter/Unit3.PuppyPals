255ba0f1a5a38e12f6c3a403034f8f688c1d4812
KanishaCarter <kanishacarter90@outlook.com>
2025-04-17 21:17:10 -0500

    passed a callback function to find which searches for a puppy with an id property matching the featPupId state value.

--- PuppyPals/src/App.jsx
+++ PuppyPals/src/App.jsx
@@ -1,28 +1,29 @@
-import {puppyList} from './data.js'
-import { useState } from 'react'
+import { puppyList } from './data.js';
+import { useState } from 'react';
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
 
-  function handleClick() {
-    // some logic here
-  }
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+  console.log(featuredPup);
 
   return (
     <div className="App">
+      {featPupId && <p>Featured Puppy ID: {featPupId}</p>}
+
       {puppies.map((puppy) => {
         return (
-          <p onClick = {() => {console.log("puppy id: ", puppy.id)}}>
-            {puppy.name}
-          </p>
-        );
+          <p 
+            
+          key={puppy.id}
+          onClick={() => {setFeatPupId(puppy.id); }} > {puppy.name}
+
+          </p> );
       })}
+  
     </div>
   );
-};
-
-
-
+}
 
 export default App;
